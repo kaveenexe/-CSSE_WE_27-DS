b427aee3f8090679200505ba711b77767c6a0916
IT21108990 <IT21108990@my.sliit.lk>
2023-04-16 19:34:08 +0530

    Added customerdb backend

---
+++ backend/customerdb-backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//Connect mongodb
+const url = process.env.MONGODB_URL;
+
+// mongoose.connect(url, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
+
+//Create conncetion
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+//Connect to port
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
