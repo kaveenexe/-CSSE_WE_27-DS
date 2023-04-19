09391a168c96b1932054f89c5cac945b04b03ba9
Ashani Rasangika Dewmini <it21016820@my.sliit.lk>
2023-04-20 00:31:09 +0530

    seller frontend add product

--- backend/seller-backend/models/Sellerdet.js
+++ backend/seller-backend/models/Sellerdet.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const SellerSchema = new Schema({
     category: {
         type : String,
-        required: true
+        required: false
     },
     title:{
         type: String,
--- frontend/src/components/SellerProduct/ProductForm.jsx
+++ frontend/src/components/SellerProduct/ProductForm.jsx
@@ -1,9 +1,41 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-export default function addproduct() {
+import React,{useState} from "react";
+import axios from "axios";
+
+export default function Addproduct() {
+
+  const[category, setCategory] = useState("");
+  const[title, setTitle] = useState("");
+  const[description, setDescription] = useState("");
+  const[price, setPrice] = useState("");
+  const[image, setImage] = useState("");
+
+
+  function sendData(e){
+    e.preventDefault();
+    
+    const NewProduct = {
+      category,
+      title,
+      description,
+      price,
+      image
+     }
+
+    axios.post("http://localhost:8003/seller/add",NewProduct).then(()=>{
+      alert("Product Added")
+      
+
+    }).catch((err)=>{
+      alert(err)
+    })
+  }
+
+
   return (
     <div>
-      <Form>
+      <Form onSubmit = {sendData}>
         <Form.Select aria-label="Default select example">
           <option>Select category</option>
           <option value="1">Beauty</option>
@@ -14,17 +46,29 @@ export default function addproduct() {
 
         <Form.Group className="mb-3">
         <Form.Label>Title</Form.Label>
-        <Form.Control type="text" placeholder="Enter the title" />
+        <Form.Control type="text" placeholder="Enter the title"
+        onChange={(e)=>{
+
+          setTitle(e.target.value);
+      }} />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>Description</Form.Label>
-        <Form.Control type="text" placeholder="Enter the description" />
+        <Form.Control type="text" placeholder="Enter the description"
+        onChange={(e)=>{
+
+          setDescription(e.target.value);
+      }} />
       </Form.Group>
 
       <Form.Group className="mb-3">
         <Form.Label>Price</Form.Label>
-        <Form.Control type="text" placeholder="Enter price" />
+        <Form.Control type="text" placeholder="Enter price" 
+        onChange={(e)=>{
+
+          setPrice(e.target.value);
+      }}/>
       </Form.Group>
 
       <Form.Group controlId="formFile" className="mb-3">
