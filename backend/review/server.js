8f7d15a00a0e2299a9406115c92fd78d44b404da
azaynm <maleeshaashenjvc@gmail.com>
2023-04-20 21:10:22 +0530

    Updated Product Review Backend

--- backend/review/server.js
+++ backend/review/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const app = express();
 
 const sellerReview = require("./routes/sellerReviewRoute"); // added
+const productReview = require("./routes/productReviewRoute"); // added
 
 var cors = require('cors')
 
@@ -21,6 +22,7 @@ app.get("/", (req, res) => res.send("Server up and running"));
 
 
 app.use("/api/sellerReview", sellerReview); // added
+app.use("/api/productReview", productReview); // added
 
 // setting up port
 const PORT = process.env.PORT || 8000;
