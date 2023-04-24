5658e22ed94786cd549ead9471ef990153b1f761
IT21108990 <IT21108990@my.sliit.lk>
2023-04-24 11:35:34 +0530

    Install some packages, home.js changed

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import GridLayout from '../components/GridLayout'
+import NavigationBar from "../components/Home/navbar";
+import HomeBanner from "../components/Home/banner";
+import Wrapper from "../components/Home/wrapper";
+import HomeProduct from "../components/Home/productcard";
+import Footer from "../components/Home/footer";
 
 const Home = () => {
   return (
     <div>
+      < HomeBanner/>
+      <Wrapper />
+      <hr />
       <GridLayout/>
-      
+      <Footer />
     </div>
   )
 }
