4fd5c4995bf507f12012741f0291c9d1e573947d
IT21108990 <IT21108990@my.sliit.lk>
2023-04-25 17:13:22 +0530

    removed unwanted files

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavigationBar from "../components/Home/navbar";
+
 import HomeBanner from "../components/Home/banner";
 import Wrapper from "../components/Home/wrapper";
 import HomeProduct from "../components/Home/productcard";
@@ -11,7 +11,7 @@ import Footer from "../components/Home/footer";
 export default function Home() {
   return (
     <div>
-      <NavigationBar />
+      
       < HomeBanner/>
       <Wrapper />
       <hr />
