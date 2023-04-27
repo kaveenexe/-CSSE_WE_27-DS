3329a0dcd4e0e10068d98c6180676c19d06161d8
IT21108990 <IT21108990@my.sliit.lk>
2023-04-27 18:14:16 +0530

    order filter by userID

--- backend/order-backend/routes/order-route.js
+++ backend/order-backend/routes/order-route.js
@@ -6,11 +6,12 @@ const {
   createOrder,
   updateOrderStatus,
   removeOrder,
+  findUserID,
 } = require("../controller/order-controller");
 
 router.get("/", getAllOrders);
 router.post("/add", createOrder);
 router.put("/update/:id", updateOrderStatus);
 router.delete("/remove/:id", removeOrder);
-
+router.get("/find/:id", findUserID);
 module.exports = router;
